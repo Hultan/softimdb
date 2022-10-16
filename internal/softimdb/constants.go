16e48daf7b2a1f00074570b60679c44a54cc3518
per <per@softteam.se>
2022-10-16 12:52:15 +0200

    Version 1.7.8 - imdb id

--- internal/softimdb/constants.go
+++ internal/softimdb/constants.go
@@ -1,7 +1,7 @@
 package softimdb
 
 const applicationTitle = "SoftImdb"
-const applicationVersion = "v 1.7.7"
+const applicationVersion = "v 1.7.8"
 const applicationCopyRight = "©SoftTeam AB, 2020"
 const listMargin = 3
 const listSpacing = 0
