efaa93f8aaa5ce826a0b8694b0a0763d36610810
per <per@softteam.se>
2023-07-24 13:14:35 +0200

    Version 2.2.0 - Version number

--- internal/softimdb/constants.go
+++ internal/softimdb/constants.go
@@ -2,7 +2,7 @@ package softimdb
 
 const (
 	applicationTitle     = "SoftImdb"
-	applicationVersion   = "v 2.1.1"
+	applicationVersion   = "v 2.2.0"
 	applicationCopyRight = "©SoftTeam AB, 2020"
 	listMargin           = 3
 	listSpacing          = 0
