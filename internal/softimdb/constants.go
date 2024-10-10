c64184a649d8f9280c842dacc4cb53d031bc028c
Per Hultqvist <per@hultqvist.nu>
2024-10-10 18:46:22 +0200

    Version 2.7.0 : Web scraping

--- internal/softimdb/constants.go
+++ internal/softimdb/constants.go
@@ -2,7 +2,7 @@ package softimdb
 
 const (
 	applicationTitle     = "SoftImdb"
-	applicationVersion   = "v 2.6.8"
+	applicationVersion   = "v 2.7.0"
 	applicationCopyRight = "©SoftTeam AB, 2020"
 	listMargin           = 3
 	listSpacing          = 0
